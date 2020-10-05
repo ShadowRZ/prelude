48d11bf7315b9b46a1c015e166990aa5a7d99d26
雨宫恋叶 <23130178+ShadowRZ@users.noreply.github.com>
2020-10-05 13:58:57 +0800

    Enable color emoji

---
+++ early-init.el
@@ -0,0 +1,2 @@
+(when (version<= emacs-version "27.1")
+  (setq xft-ignore-color-fonts nil))
--- personal/misc.el
+++ personal/misc.el
@@ -6,3 +6,4 @@
 (scroll-bar-mode -1)
 (prelude-require-packages '(perspective hydra ivy-hydra))
 (persp-mode)
+(set-fontset-font t '(#x1f300 . #x1fad0) "Noto Color Emoji")
