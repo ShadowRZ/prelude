b16569f23b2ba2941deafaea540353400c61c94c
雨宫恋叶 <23130178+ShadowRZ@users.noreply.github.com>
2020-10-05 12:31:04 +0800

    Include package semantic.

---
+++ personal/semantic.el
@@ -0,0 +1,6 @@
+(require 'semantic)
+(global-semanticdb-minor-mode)
+(global-semantic-idle-scheduler-mode)
+(global-semantic-highlight-func-mode)
+(global-semantic-stickyfunc-mode -1)
+(semantic-mode)
